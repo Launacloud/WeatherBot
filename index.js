f65603c51c287477276ee1f22ce2bd5469859f95
Leandro Watanabe - DESKTOP <setsuo.leandro@gmail.com>
2021-12-13 19:11:40 -0300

    Refactor: Adicionando a tratativa para multiplas entradas de cidade

--- index.js
+++ index.js
@@ -19,41 +19,43 @@ bot.on('messageCreate', async (msg) => {
   
   if (foundWord && ('!weather'||'!clima')){
 
-    const [lang,city] = msg.content.replace(/\s+/, '\x01').split('\x01')
-    
-    if(lang == '!clima'){
-      if(city == "info"){
-        msg.channel.send(
-          "Comando válido: \"!clima [cidade]\"\n[cidade] deve ser substituida por uma cidade válida sem acento"
-        )
-      }else{
-        Weather(city, "pt_br").then(weather => {        
-          if(weather.desc.descp=="unavailable"){
-            msg.channel.send(
-              "Cidade invalida\nDigite \"!clima info\" para mais informações"
-            )
-          }else{
-            msg.channel.send(
-              `${weather.desc.descp} em ${city} \nA temperatura é ${weather.desc.temp} mas a sensação é de ${weather.desc.feel}`
-            )};
-        })
-      }
-    }else if(lang == '!weather'){
-      if(city == "info"){
-        msg.channel.send(
-          "Valid command: \"!weather [city]\"\n[city] must be a valid city"
-        )
-      }else{
-        Weather(city, "en").then(weather => {        
-          if(weather.desc.descp=="unavailable"){
-            msg.channel.send(
-              "Invalid City\nType \"!weather info\" for more information"
-            )
-          }else{
-            msg.channel.send(
-              `${weather.desc.descp} in ${city} \nThe temperature is ${weather.desc.temp} but feels like ${weather.desc.feel}`
-            )};
-        })
+    const [lang, citiesList] = msg.content.replace(/\s+/, '\x01').split('\x01')
+    const cities = citiesList.split(', ')
+    for ( const index in cities){
+      if(lang == '!clima'){
+        if(cities[index] == "info"){
+          msg.channel.send(
+            "Comando válido: \"!clima [cidade]\"\n[cidade] deve ser substituida por uma cidade válida sem acento\n"
+          )
+        }else{
+          Weather(cities[index], "pt_br").then(weather => {        
+            if(weather.desc.descp=="unavailable"){
+              msg.channel.send(
+                "Cidade invalida\nDigite \"!clima info\" para mais informações\n"
+              )
+            }else{
+              msg.channel.send(
+                `${weather.desc.descp} em ${cities[index]} \nA temperatura é ${weather.desc.temp} mas a sensação é de ${weather.desc.feel}\n`
+              )};
+          })
+        }
+      }else if(lang == '!weather'){
+        if(cities[index] == "info"){
+          msg.channel.send(
+            "Valid command: \"!weather [city]\"\n[city] must be a valid city\n"
+          )
+        }else{
+          Weather(cities[index], "en").then(weather => {        
+            if(weather.desc.descp=="unavailable"){
+              msg.channel.send(
+                "Invalid City\nType \"!weather info\" for more information\n"
+              )
+            }else{
+              msg.channel.send(
+                `${weather.desc.descp} in ${cities[index]} \nThe temperature is ${weather.desc.temp} but feels like ${weather.desc.feel}\n`
+              )};
+          })
+        }
       }
     }
   }
